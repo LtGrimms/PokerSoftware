8dbf21e7ce29e12ae5321c46fa4023e77a1ee3b3
Madison Lowe <madison@morpheusmedical.net>
2015-09-06 23:42:02 -0600

    Established a connection with Tonys MySQL

--- poker-file-parser.js
+++ poker-file-parser.js
@@ -12,20 +12,27 @@ fs.readFile(filename, 'utf8', function(err, data) {
 
     // Now that we have the data, make a database connection and insert it
     // (Just getting this working for one hand for now - will have to put it in a loop later)
-    // var connection = mysql.createConnection({
-    //     host: 'localhost', // we will probably have to put the port in here too.. 3306?
-    //     user: '', // I don't remember your user name
-    //     password: 'bone crusher', // or something
-    //     database: 'poker' // is that what we called it?
-    // });
+    var connection = mysql.createConnection({
+        host: '192.168.0.4',
+        port: 3306,// we will probably have to put the port in here too.. 3306?
+        user: 'root', // I don't remember your user name
+        password: 'BreakUrBones', // or something
+        database: 'poker' // is that what we called it?
+    });
 
-    // connection.connect();
-
-    // // do some inserting and stuff
-    // connection.query(hand, function(err, rows, fields) {
-    //     console.error(err);
-    //     console.log(rows);
-    //     console.log(fields);
-    //     connection.end();
-    // });
+    connection.connect(function(err, result) {
+        if (err) {
+            console.error("Could not connect to database: " + err);
+            connection.end();
+            return;
+        }
+        connection.query("",hand, function(err, result) {
+            if (err) {
+                console.error("Could not insert data: " + err);
+                connection.end();
+                return;
+            }
+            connection.end();
+        });
+    });
 });
