c4c1370e65c1019592bfc95b3a7cce415f117be2
LtGrimms <alcridla@gmail.com>
2015-09-06 23:06:13 -0600

    new testing files and their expected output

---
+++ test/testHands/expected-output-hand2.js
@@ -0,0 +1,239 @@
+module.exports = {
+    hand: {
+        number: '139420987309',
+        timeStamp: '2015/08/15 14:41:03 ET',
+        tournament: '1292680929',
+        anteAmount: '15',
+        smallBlindAmount: 60,
+        bigBlindAmount: 120,
+        numberOfPlayers: 9
+    },
+    seats: [
+        {
+            handNumber: '139420987309',
+            name: 'gits1980',
+            chips: '1780',
+            position: 8,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'rastamanooo',
+            chips: '2135',
+            position: 9,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'liamkid',
+            chips: '1664',
+            position: 1,
+            winnings: '735',
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'R@nTAnpl@N',
+            chips: '2546',
+            position: 2,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'Fakhish',
+            chips: '4579',
+            position: 3,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'mecco',
+            chips: '3384',
+            position: 4,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'LtGrimms',
+            chips: '6491',
+            position: 5,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'Adios89',
+            chips: '2031',
+            position: 6,
+            winnings: null,
+            sidepots: null
+        }, {
+            handNumber: '139420987309',
+            name: 'kartrid',
+            chips: '2058',
+            position: 7,
+            winnings: null,
+            sidepots: null
+        }
+    ],
+    board: {
+        handNumber: '139420987309',
+        flop1: null,
+        flop2: null,
+        flop3: null,
+        turn: null,
+        river: null
+    },
+    holeCards: [
+        {
+            handNumber: '139420987309', name: 'LtGrimms', card1: 'Ac', card2: '7s'
+        }
+    ],
+    actions: [
+        {
+            handNumber: '139420987309',
+            name: 'gits1980',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'rastamanooo',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'liamkid',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'R@nTAnpl@N',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'Fakhish',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'mecco',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'LtGrimms',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'Adios89',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'kartrid',
+            round: 'preflop',
+            type: 'post ante',
+            amount: 15,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'liamkid',
+            round: 'preflop',
+            type: 'post blind',
+            amount: 60,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'R@nTAnpl@N',
+            round: 'preflop',
+            type: 'post blind',
+            amount: 120,
+            currentBetSize: null
+        }, {
+            handNumber: '139420987309',
+            name: 'Fakhish',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'mecco',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'LtGrimms',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'Adios89',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'kartrid',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'gits1980',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'rastamanooo',
+            round: 'preflop',
+            type: 'raise',
+            amount: 120,
+            currentBetSize: 120
+        }, {
+            handNumber: '139420987309',
+            name: 'liamkid',
+            round: 'preflop',
+            type: 'raise',
+            amount: 360,
+            currentBetSize: 240
+        }, {
+            handNumber: '139420987309',
+            name: 'R@nTAnpl@N',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 600
+        }, {
+            handNumber: '139420987309',
+            name: 'rastamanooo',
+            round: 'preflop',
+            type: 'fold',
+            amount: null,
+            currentBetSize: 600
+        }
+    ]};
