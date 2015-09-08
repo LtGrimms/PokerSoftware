ea2b7acf38c314f247219c63a5bd265d0ffece18
LtGrimms <alcridla@gmail.com>
2015-09-07 22:17:13 -0600

    changed data types for parsed values

--- test/testHands/expected-output-hand1.js
+++ test/testHands/expected-output-hand1.js
@@ -14,64 +14,64 @@ module.exports = {
             name: 'gits1980',
             chips: '1765',
             position: 7,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         }, {
             handNumber: '139421020234',
             name: 'rastamanooo',
             chips: '1880',
             position: 8,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         }, {
             handNumber: '139421020234',
             name: 'liamkid',
             chips: '2144',
             position: 9,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         }, {
             handNumber: '139421020234',
             name: 'R@nTAnpl@N',
             chips: '2411',
             position: 1,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         }, {
             handNumber: '139421020234',
             name: 'Fakhish',
             chips: '4564',
             position: 2,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         },{
             handNumber: '139421020234',
             name: 'mecco',
             chips: '3369',
             position: 3,
             winnings: '4371',
-            sidepots: null
+            sidepots: 0
         },{
             handNumber: '139421020234',
             name: 'LtGrimms',
             chips: '6476',
             position: 4,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         },{
             handNumber: '139421020234',
             name: 'Adios89',
             chips: '2016',
             position: 5,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         },{
             handNumber: '139421020234',
             name: 'kartrid',
             chips: '2043',
             position: 6,
-            winnings: null,
-            sidepots: null
+            winnings: 0,
+            sidepots: 0
         }
     ],
     board: {
@@ -94,77 +94,77 @@ module.exports = {
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'rastamanooo',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'liamkid',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'R@nTAnpl@N',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'Fakhish',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'mecco',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'LtGrimms',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'Adios89',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'kartrid',
             round: 'preflop',
             type: 'post ante',
             amount: 15,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'R@nTAnpl@N',
             round: 'preflop',
             type: 'post blind',
             amount: 60,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'Fakhish',
             round: 'preflop',
             type: 'post blind',
             amount: 120,
-            currentBetSize: null
+            currentBetSize: 0
         }, {
             handNumber: '139421020234',
             name: 'mecco',
