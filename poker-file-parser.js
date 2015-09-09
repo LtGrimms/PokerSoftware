daf6f8e52b6775b2f30482c4d70f45b2e1937553
Madison Lowe <madison@morpheusmedical.net>
2015-09-08 21:17:24 -0600

    loop through the whole file and parse all the hands

--- poker-file-parser.js
+++ poker-file-parser.js
@@ -8,24 +8,16 @@ fs.readFile(filename, 'utf8', function(err, data) {
     if (err) {
         console.error(err);
     }
-    var hand = hand_parser(data);
 
     // Now that we have the data, make a database connection and insert it
-    // (Just getting this working for one hand for now - will have to put it in a loop later)
     var connection = mysql.createConnection({
         host: '192.168.0.4',
-        port: 3306,// we will probably have to put the port in here too.. 3306?
-        user: 'root', // I don't remember your user name
-        password: 'BreakUrBones', // or something
-        database: 'poker' // is that what we called it?
+        port: 3306,
+        user: 'root',
+        password: 'BreakUrBones',
+        database: 'poker'
     });
 
-    connection.connect(function(err, result) {
-        if (err) {
-            console.error("Could not connect to database: " + err);
-        }
-        console.log("Connected successfully");
-    });
 
     function insert_object(table, object) {
         connection.query("INSERT INTO " + table + " SET ?", object, function(err, result) {
@@ -37,17 +29,30 @@ fs.readFile(filename, 'utf8', function(err, data) {
         });
     }
 
-    insert_object("hand", hand.hand);
-    var i;
-    for (i = 0; i < hand.seats.length; i++) {
-        insert_object("seat", hand.seats[i]);
-    }
-    insert_object("board", hand.board);
-    for (i = 0; i < hand.holeCards.length; i++) {
-        insert_object("holeCards", hand.holeCards[i]);
-    }
-    for (i = 0; i < hand.actions.length; i++) {
-        insert_object("action", hand.actions[i]);
+    connection.connect(function(err, result) {
+        if (err) {
+            console.error("Could not connect to database: " + err);
+        }
+        console.log("Connected successfully");
+    });
+
+    var hands = data.split("\n\n\n\n");
+    for (var j = 0; j < hands.length; j++) {
+        var i;
+        if (!hands[j]) continue;
+        var hand = hand_parser(hands[j]);
+        console.log("parsed hand: " + hand.hand.number);
+        insert_object("hand", hand.hand);
+        for (i = 0; i < hand.seats.length; i++) {
+            insert_object("seat", hand.seats[i]);
+        }
+        insert_object("board", hand.board);
+        for (i = 0; i < hand.holeCards.length; i++) {
+            insert_object("holeCards", hand.holeCards[i]);
+        }
+        for (i = 0; i < hand.actions.length; i++) {
+            insert_object("action", hand.actions[i]);
+        }
     }
     connection.end();
 });
